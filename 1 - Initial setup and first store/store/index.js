1e8d8da5ea16fda038a05898e58bba94abb2136b
Oscar Bellon <oscar.bellon@edreamsodigeo.com>
2021-02-08 09:45:23 +0100

    1 - Initial setup and first store

---
+++ 1 - Initial setup and first store/store/index.js
@@ -0,0 +1,25 @@
+import {
+    createStore,
+} from 'redux';
+import {rootReducer} from './reducers/index';
+
+const connectedModels = [];
+const store = createStore(rootReducer);
+
+store.subscribe(() => {
+    connectedModels.forEach(triggerModelChange);
+});
+
+const triggerModelChange = (modelClass) => {
+    if (typeof modelClass.onStoreChange === 'function') {
+        modelClass.onStoreChange(store.getState());        
+    }
+};
+
+export const connectModelToRedux = (modelClass) => {
+    console.log(`Adding ${modelClass.id} to the connectedModels`);
+    connectedModels.push(modelClass);
+    return modelClass;
+};
+
+console.log('Store initialized!')
